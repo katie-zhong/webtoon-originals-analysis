e55a04e3143254af7530885172ed26eb2417f123
katie-zhong <tokatiezhong@gmail.com>
2022-07-27 11:04:25 -0400

    Fixed graph bugs and datatable knitting error

--- numOfOriginalsTable.R
+++ numOfOriginalsTable.R
@@ -6,10 +6,10 @@ num_table <- read_rds("webtoon-data.rds") |>
   rename("Genre" = genre,
          "Number of Originals" = num)
 
-datatable(num_table,
+num_datatable <- datatable(num_table,
           options = list(scrollx = TRUE,
                          editable = FALSE,
                          pageLength = 16),
           caption = "Table 1: Genre distribution in the sample dataset of 774 WEBTOONs.")
 
-write_rds(num_table, "originals_per_genre.rds")
+write_rds(num_datatable, "originals_per_genre.rds")
